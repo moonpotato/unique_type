9852eb2747938a1db303c4e7f74f9cc4ba458afd
moonpotato <moonpotato@users.noreply.github.com>
2017-10-19 00:09:34 +0800

    Create initial unique_type class

---
+++ unique_type.hpp
@@ -0,0 +1,28 @@
+#ifndef MPT_UNIQUE_TYPE_HPP
+#define MPT_UNIQUE_TYPE_HPP
+
+namespace mpt
+{
+    template <typename T, typename Tag>
+    class unique_type
+    {
+        using Unique = unique_type<T, Tag>;
+
+    public:
+        unique_type() = default;
+        explicit unique_type(const T& val) : value(val) {}
+        explicit unique_type(T&& val) : value(val) {}
+
+        explicit operator T() const { return value; }
+        explicit operator T&() { return value; }
+    
+    private:
+        T value;
+    };
+}
+
+#define MPT_UNIQUE_TYPE(name, type) \
+    struct name##_tag {}; \
+    using name = mpt::unique_type<type, name##_tag>
+
+#endif // MPT_UNIQUE_TYPE_HPP
